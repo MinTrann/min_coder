d909e30a28fa7456cbdc0ceea3ea421c4d1f32a4
Min Nè <mannne...@nhatmin.lan>
2024-06-06 22:41:23 +0700

    minn

---
+++ asset/js/play-music.js
@@ -0,0 +1,39 @@
+const btn = document.querySelector('.btnplay')
+const imgPlay = document.querySelector('.imgplay')
+const btnOff = document.querySelector('.btnoff')
+const playBtn = document.querySelector('.play-btn')
+const playIcon = document.querySelector('.play-icon')
+const progressLine = document.querySelector('.progress-line')
+const audio = new Audio('./asset/music/mong-rang-em-luon-manh-me-vuot-qua-duoc-bao-thang-tram-huy-vac-lyrics-video.mp3')
+playBtn.onclick = function(){
+    const isPlaying = !audio.paused
+    playIcon.name = isPlaying ? 'play': 'pause'
+    if(isPlaying){
+        audio.pause()
+        imgPlay.classList.remove('ani-img')
+    }
+    else{
+        audio.play()
+        interval = setInterval(() =>{
+                    progressBar()
+        },1000)
+        imgPlay.classList.add('ani-img') 
+    }
+}
+// * 
+function progressBar(){
+    const ducation = audio.duration
+    let currentTime = audio.currentTime 
+    if(currentTime >= ducation){
+        audio.currentTime = 0
+        imgPlay.classList.remove('ani-img')
+        playIcon.name = 'play'
+    }
+    progressLine.style.width =(currentTime /ducation) * 100 + '%'
+}
+//
+console.log(alert("Khả Khả <3 "));
+
+
+   
+  
